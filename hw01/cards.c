c157b258e7929158142802a96561624518a48b0e
Noam Rubin <rubin.m.noam@gmail.com>
2014-01-30 21:34:56 -0500

    my cards.c compiles without any warnings at all

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,7 +1,7 @@
 /*
 Software Systems Homework 1
 Noam Rubin
-1/28/14
+1/30/14
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +25,7 @@ A return value of 0 indicates that an error occured in parsing the card number.
 int get_card_val(char *card_name)
 {
     int val = 0;
-    switch (card_name[0])
+    switch (*card_name)
     {
     case 'K':
     case 'Q':
@@ -51,39 +51,40 @@ int get_card_val(char *card_name)
 Adjusts the current card count
 
 val: the value of the card
-count: the current card count in the game
+*count: pointer to the current card count in the game
 */
-void update_card_count(int val, int count)
+void update_card_count(int val, int* count)
 {
     if ((val > 2) && (val < 7))
     {
-        count++;
+        *count = *count + 1;
     }
     else if (val == 10)
     {
-        count--;
+        *count = *count - 1;
     }
 }
 /*
 Runs the main game loop
 */
-void main_loop(void)
+void loop(void)
 {
-    int val, count, STOP;
-    val = count = STOP = 0;
-    char[3] card_name;
+    int val = 0;
+    int count = 0;
+    int STOP = 0;
+    char card_name;
     while (!STOP)
     {
         card_name = card_name_reader();
-        if (card_name[0] == 'X')
+        if (card_name == 'X')
         {
             printf("Game Over! The final count is: %i\n", count);
             break;
         }
-        val = get_card_val(card_name);
+        val = get_card_val(&card_name);
         if (val != 0)
         {
-            update_card_count(val, count)
+            update_card_count(val, &count);
             printf("The current count is: %i\n", count);
         }
     }
@@ -91,50 +92,6 @@ void main_loop(void)
 
 int main()
 {
-    main_loop();
+    loop();
     return 0;
 }
-
-
-// int main()
-// {
-//     char card_name[3];
-//     int count = 0;
-//     while ( card_name[0] != 'X')
-//     {
-//         puts("Enter the card_name: ");
-//         scanf("%2s", card_name);
-//         int val = 0;
-//         switch (card_name[0])
-//         {
-//         case 'K':
-//         case 'Q':
-//         case 'J':
-//             val = 10;
-//             break;
-//         case 'A':
-//             val = 11;
-//             break;
-//         case 'X':
-//             continue;
-//         default:
-//             val = atoi(card_name);
-//             if ((val < 1) || (val > 10))
-//             {
-//                 puts("I don't understand that value!");
-//                 continue;
-//             }
-//         }
-//         if ((val > 2) && (val < 7))
-//         {
-//             count++;
-//         }
-//         else if (val == 10)
-//         {
-//             count--;
-//         }
-//         printf("Current count: %i\n", count);
-//     }
-//     printf("final count: %i\n", count);
-//     return 0;
-// }
